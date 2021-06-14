ab0f1b14e2acf105cb7de7b922c4e34681a034ea
mr-robot <mahendrarathore1742@gmail.com>
2021-06-14 18:01:46 +0530

    add code

--- src/app.js
+++ src/app.js
@@ -2,65 +2,14 @@ const express=require('express');
 require('./db/conn');
 const Student= require('./models/students')
 const app= express();
-
+const router = require('./router/student');
 const port = process.env.PORT || 4000;
 app.use(express.json());
 
-app.get('/',(req,res) =>{
-	res.send("Home");
-})
-
-// app.post("/student",(req,res) =>{
-// 	const user= new Student(req.body);
-// 	user.save()
-// 	.then(() =>{
-// 		res.send(user)
-// 	})
-// 	.catch((e) =>{
-// 		console.log(e);
-// 	})
-// })
-
-// insert data in database using api
-app.post('/student',async (req,res) => {
-	const user = new Student(req.body);
-	const userdata = await user.save();
-	res.send(userdata);
-})
 
 
-app.get('/student', async (req,res) =>{
-	try{
-		const studentdata = await Student.find();
-		res.send(studentdata);
-	}
-	catch(e){
-		res.send(e);
-	}
-} )
+app.use(router);
 
-//get single data 
-app.get('/student/:id', async (req,res) =>{
-	try{
-		const _id=req.params.id;
-		const studentdata= await Student.findById(_id);
-		res.send(studentdata);
-	}
-	catch(e){
-		res.send(e);
-	}
-})
-
-app.patch('/student/:id', async (req,res) =>{
-	try{
-		const _id=req.params.id;
-		const studentdata= await Student.findByIdAndUpdate(_id,req.body);
-		res.send(studentdata);
-	}
-	catch(e){
-		res.send(e);
-	}
-})
 
 app.listen(port,() =>{
 	console.log('its successfull running')
