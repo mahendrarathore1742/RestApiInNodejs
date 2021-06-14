f7949ccfcc4088020243f7612f5e21d675962ca3
mr-robot <mahendrarathore1742@gmail.com>
2021-06-14 12:58:14 +0530

    add more code

--- src/app.js
+++ src/app.js
@@ -10,17 +10,56 @@ app.get('/',(req,res) =>{
 	res.send("Home");
 })
 
-app.post("/student",(req,res) =>{
+// app.post("/student",(req,res) =>{
+// 	const user= new Student(req.body);
+// 	user.save()
+// 	.then(() =>{
+// 		res.send(user)
+// 	})
+// 	.catch((e) =>{
+// 		console.log(e);
+// 	})
+// })
 
-	console.log(req.body)
-	const user= new Student(req.body);
-	user.save()
-	.then(() =>{
-		res.send(user)
-	})
-	.catch((e) =>{
-		console.log(e);
-	})
+// insert data in database using api
+app.post('/student',async (req,res) => {
+	const user = new Student(req.body);
+	const userdata = await user.save();
+	res.send(userdata);
+})
+
+
+app.get('/student', async (req,res) =>{
+	try{
+		const studentdata = await Student.find();
+		res.send(studentdata);
+	}
+	catch(e){
+		res.send(e);
+	}
+} )
+
+//get single data 
+app.get('/student/:id', async (req,res) =>{
+	try{
+		const _id=req.params.id;
+		const studentdata= await Student.findById(_id);
+		res.send(studentdata);
+	}
+	catch(e){
+		res.send(e);
+	}
+})
+
+app.patch('/student/:id', async (req,res) =>{
+	try{
+		const _id=req.params.id;
+		const studentdata= await Student.findByIdAndUpdate(_id,req.body);
+		res.send(studentdata);
+	}
+	catch(e){
+		res.send(e);
+	}
 })
 
 app.listen(port,() =>{
